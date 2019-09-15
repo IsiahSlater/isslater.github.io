0620d2f4a54a92c5ce68274d5f3cdf962cee186d
Isslater182 <mr.bwee@gmail.com>
2019-09-15 15:39:48 -0500

    Standardized Button and Fixed Mobile Layout
    Made it so that only the phone version displays the overhead navbar while other versions rely on the dropdown menu
    Furthermore, the dropdown size was changed to use the same measurements for each layout change

--- WebsiteFunctions.js
+++ WebsiteFunctions.js
@@ -1,10 +1,15 @@
-var windowHeight = window.innerHeight;
-window.addEventListener("wheel", function (event) {
+function smoothScroll() {
     'use strict';
-    if (event.deltaY < 0) {
-        window.scrollBy(0, -windowHeight);
-
-    } else if (event.deltaY > 0) {
-        window.scrollBy(0, windowHeight);
-    }
-});
+    var windowHeight = window.document.getElementById("Title").offsetHeight;
+    window.addEventListener("wheel", function (event) {
+        if (event.deltaY < 0) {
+            window.scrollBy(0, -windowHeight);
+        } else if (event.deltaY > 0) {
+            window.scrollBy(0, windowHeight);
+        }
+    });
+}
+/* A switch case in which it scrolls to the next section, depending on which section it is currently on 
+var divArray = [document.getElementById("Title"), document.getElementById("LBlue"), document.getElementById("Pink"),
+                document.getElementById("Yelow"), document.getElementById("Tan")];
+                */
