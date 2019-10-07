eb7cf29c3857b9403f4fdf60a4340d44de9ba692
Isslater182 <mr.bwee@gmail.com>
2019-10-07 17:04:02 -0500

    Small color fix + redoing scroll code
    I am redoing the javascript to allow for more precise scrolling

--- WebsiteFunctions.js
+++ WebsiteFunctions.js
@@ -1,15 +1,30 @@
 function smoothScroll() {
     'use strict';
-    var windowHeight = window.document.getElementById("Title").offsetHeight;
+    
+    var windowHeight = window.document.getElementById("Title").offsetHeight,
+        divArray = [document.getElementById("Title"), document.getElementById("LBlue"), document.getElementById("Pink"), document.getElementById("Yellow"), document.getElementById("Tan")],
+        arrayCount = 0;
+    
     window.addEventListener("wheel", function (event) {
-        if (event.deltaY < 0) {
-            window.scrollBy(0, -windowHeight);
-        } else if (event.deltaY > 0) {
-            window.scrollBy(0, windowHeight);
+        if (event.deltaY <= -1) {
+            if (arrayCount !== 0) {
+                arrayCount -= 1;
+                window.scrollTo(0, divArray[arrayCount].offsetTop);
+            }
+            /*window.scrollBy(0, -windowHeight);*/
+        } else if (event.deltaY >= 1) {
+            if (arrayCount < divArray.size) {
+                arrayCount += 1;
+                window.scrollTo(0, divArray[arrayCount].offsetTop);
+            }
+            /*window.scrollBy(0, windowHeight);*/
         }
     });
 }
-/* A switch case in which it scrolls to the next section, depending on which section it is currently on 
-var divArray = [document.getElementById("Title"), document.getElementById("LBlue"), document.getElementById("Pink"),
-                document.getElementById("Yelow"), document.getElementById("Tan")];
-                */
+/* A switch case in which it scrolls to the next section, depending on which section it is currently on */
+
+                /*Assume mouse start in location 0 of array ("title")
+                when scroll detected, increase array element by 1
+                Set window location to top of that array element*/
+
+            
