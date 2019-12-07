323ad3e5104719fbf348b1d032cfeca3e9c4aaef
Isslater182 <mr.bwee@gmail.com>
2019-12-07 02:19:25 -0600

    New nav menu
    replaced the boring css version with an updated nav menu that utilizes css and javascript to smoothly project a menu overlay onto the screen.

--- WebsiteFunctions.js
+++ WebsiteFunctions.js
@@ -1,30 +1,42 @@
+function openNav() {
+  document.getElementById("menu").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("menu").style.width = "0%";
+}
+
 function smoothScroll() {
     'use strict';
-    
-    var windowHeight = window.document.getElementById("Title").offsetHeight,
-        divArray = [document.getElementById("Title"), document.getElementById("LBlue"), document.getElementById("Pink"), document.getElementById("Yellow"), document.getElementById("Tan")],
-        arrayCount = 0;
-    
+    var windowHeight = window.document.getElementById("Title").offsetHeight;
+    var divArray = [document.getElementById("Title"), document.getElementById("LBlue"), document.getElementById("Pink"), document.getElementById("Yellow"), document.getElementById("Tan")];
+    var arrayCount = 0;
+
     window.addEventListener("wheel", function (event) {
-        if (event.deltaY <= -1) {
-            if (arrayCount !== 0) {
-                arrayCount -= 1;
-                window.scrollTo(0, divArray[arrayCount].offsetTop);
-            }
-            /*window.scrollBy(0, -windowHeight);*/
-        } else if (event.deltaY >= 1) {
-            if (arrayCount < divArray.size) {
-                arrayCount += 1;
-                window.scrollTo(0, divArray[arrayCount].offsetTop);
-            }
-            /*window.scrollBy(0, windowHeight);*/
+      for(var i = 0; i < divArray.length; i++) {
+        if(divArray[i].onmouseover) {
+          if (event.deltaY <= -1) {
+              if (arrayCount !== 0) {
+                  arrayCount -= 1;
+                  window.scrollTo(0, divArray[arrayCount].offsetTop);
+              }
+              /*window.scrollBy(0, -windowHeight);*/
+          } else if (event.deltaY >= 1) {
+              if (arrayCount < divArray.size) {
+                  arrayCount += 1;
+                  window.scrollTo(0, divArray[arrayCount].offsetTop);
+              }
+              /*window.scrollBy(0, windowHeight);*/
+          }
         }
+      }
+
     });
 }
+
+
 /* A switch case in which it scrolls to the next section, depending on which section it is currently on */
 
                 /*Assume mouse start in location 0 of array ("title")
                 when scroll detected, increase array element by 1
                 Set window location to top of that array element*/
-
-            
